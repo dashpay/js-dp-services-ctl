7998b61114eff1feff806998428e361331047867
Djavid Gabibiyan <djavidoff@gmail.com>
2018-07-30 20:22:08 +0500

    increased timeouts for aws auth test

--- test/integration/docker/getAwsEcrAuthorizationToken.js
+++ test/integration/docker/getAwsEcrAuthorizationToken.js
@@ -1,6 +1,8 @@
 const getAwsEcrAuthorizationToken = require('../../../lib/docker/getAwsEcrAuthorizationToken');
 
-describe('getAwsEcrAuthorizationToken', () => {
+describe('getAwsEcrAuthorizationToken', function main() {
+  this.timeout(10000);
+
   it('should get the authorization', async () => {
     const authorization = await getAwsEcrAuthorizationToken(process.env.AWS_DEFAULT_REGION);
     expect(authorization.username).to.exist();
