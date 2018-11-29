10234c274f1fb609b5b86120bfec0b724fbb9d19
Djavid Gabibiyan <djavidoff@gmail.com>
2018-11-29 23:33:08 +0500

    [DD-278] Don't fetch images every time
    * initial approach to "do not pull an image every time"
    
    * integration test for Image pulling
    
    * fix linter errors

--- test/integration/docker/Image.js
+++ test/integration/docker/Image.js
@@ -1,15 +1,29 @@
+const Docker = require('dockerode');
 const DashCoreOptions = require('../../../lib/services/dashCore/DashCoreOptions');
 const MongoDbOptions = require('../../../lib/services/mongoDb/MongoDbOptions');
 const getAwsEcrAuthorizationToken = require('../../../lib/docker/getAwsEcrAuthorizationToken');
 const Image = require('../../../lib/docker/Image');
 
 describe('Image', function main() {
-  this.timeout(20000);
+  this.timeout(200000);
 
+  let docker;
+  beforeEach(function beforeEach() {
+    docker = new Docker();
+    this.sinon.spy(docker, 'pull');
+  });
 
   it('should pull image without authentication', async () => {
     const options = new MongoDbOptions();
     const imageName = options.getContainerImageName();
+
+    const dockerImage = await docker.getImage(imageName);
+    try {
+      await dockerImage.remove({ v: true });
+    } catch (e) {
+      // skipping
+    }
+
     const image = new Image(imageName);
     await image.pull();
   });
@@ -17,8 +31,34 @@ describe('Image', function main() {
   it('should pull image with authentication', async () => {
     const options = new DashCoreOptions();
     const imageName = options.getContainerImageName();
+
+    const dockerImage = await docker.getImage(imageName);
+    try {
+      await dockerImage.remove({ v: true });
+    } catch (e) {
+      // skipping
+    }
+
     const authorizationToken = await getAwsEcrAuthorizationToken(options.getAwsOptions());
     const image = new Image(imageName, authorizationToken);
     await image.pull();
+  });
+
+  it('should pull image only if it is not present', async () => {
+    const imageName = 'alpine';
+    const dockerImage = await docker.getImage(imageName);
+    try {
+      await dockerImage.remove({ v: true });
+    } catch (e) {
+      // skipping
+    }
+
+    const image = new Image(imageName);
+    image.docker = docker;
+
+    await image.pull();
+    await image.pull();
+
+    expect(docker.pull).to.be.calledOnce();
   });
 });
