b309cf30b6f9b6b1cf51ee3da7bfd0c14044532f
Djavid Gabibiyan <djavidoff@gmail.com>
2019-01-25 13:16:25 +0500

    implement tests for mocha hooks to test IPFS clean

--- test/integration/mocha/startIPFS.js
+++ test/integration/mocha/startIPFS.js
@@ -5,29 +5,58 @@ describe('startIPFS', function main() {
 
   describe('One instance', () => {
     let ipfsAPI;
+    let actualTrueObject;
     startIPFS().then((_instance) => {
       ipfsAPI = _instance.getApi();
     });
 
     it('should start one instance', async () => {
-      const actualTrueObject = await ipfsAPI.block.put(Buffer.from('{"true": true}'));
+      actualTrueObject = await ipfsAPI.block.put(Buffer.from('{"true": true}'));
       const expectedTrueObject = await ipfsAPI.block.get(actualTrueObject.cid);
       expect(expectedTrueObject.data).to.be.deep.equal(actualTrueObject.data);
+    });
+
+    it('should not have any of the previous data', async () => {
+      const result = await Promise.race([
+        ipfsAPI.block.get(actualTrueObject.cid),
+        new Promise(resolve => setTimeout(() => resolve(false), 2000)),
+      ]);
+      expect(result).to.be.equal(false);
     });
   });
 
   describe('Three instances', () => {
     let ipfsAPIs;
+    let actualTrueObject;
     startIPFS.many(3).then((_instances) => {
       ipfsAPIs = _instances.map(instance => instance.getApi());
     });
 
     it('should start many instances', async () => {
-      const actualTrueObject = await ipfsAPIs[0].block.put(Buffer.from('{"true": true}'));
+      actualTrueObject = await ipfsAPIs[0].block.put(Buffer.from('{"true": true}'));
 
       for (let i = 1; i < 3; i++) {
         const expectedTrueObject = await ipfsAPIs[i].block.get(actualTrueObject.cid);
         expect(expectedTrueObject.data).to.be.deep.equal(actualTrueObject.data);
+      }
+    });
+
+    it('should not have any of the previous data', async () => {
+      for (let i = 1; i < 3; i++) {
+        const result = await Promise.race([
+          ipfsAPIs[i].block.get(actualTrueObject.cid),
+          new Promise(resolve => setTimeout(() => resolve(false), 2000)),
+        ]);
+        expect(result).to.be.equal(false);
+      }
+    });
+
+    it('should have nodes connected event after clean', async () => {
+      const anotherObject = await ipfsAPIs[0].block.put(Buffer.from('{"true": true}'));
+
+      for (let i = 1; i < 3; i++) {
+        const expectedObject = await ipfsAPIs[i].block.get(anotherObject.cid);
+        expect(expectedObject.data).to.be.deep.equal(anotherObject.data);
       }
     });
   });
