5816fdf5c75dac2847aa9de836fc0ceedcf32b70
Djavid Gabibiyan <djavidoff@gmail.com>
2019-02-07 11:45:39 +0500

    remove private registry link from Image test

--- test/integration/docker/Image.js
+++ test/integration/docker/Image.js
@@ -3,13 +3,17 @@ const DashCoreOptions = require('../../../lib/services/dashCore/DashCoreOptions'
 const getAwsEcrAuthorizationToken = require('../../../lib/docker/getAwsEcrAuthorizationToken');
 const Image = require('../../../lib/docker/Image');
 
+const stream = require('stream');
+
 describe('Image', function main() {
   this.timeout(200000);
 
   let docker;
+  let mockedStream;
   beforeEach(function beforeEach() {
     docker = new Docker();
-    this.sinon.spy(docker, 'pull');
+    mockedStream = new stream.Readable();
+    mockedStream._read = function(size) { /* stub */ };
   });
 
   it('should pull image without authentication', async () => {
@@ -26,23 +30,25 @@ describe('Image', function main() {
     await image.pull();
   });
 
-  it('should pull image with authentication', async () => {
+  it.only('should pull image with authentication', async function() {
     const options = new DashCoreOptions();
-    const imageName = '103738324493.dkr.ecr.us-west-2.amazonaws.com/dashevo/node:10-alpine';
-
-    const dockerImage = await docker.getImage(imageName);
-    try {
-      await dockerImage.remove({ v: true });
-    } catch (e) {
-      // skipping
-    }
+    const imageName = 'private/image:name';
 
     const authorizationToken = await getAwsEcrAuthorizationToken(options.getAwsOptions());
     const image = new Image(imageName, authorizationToken);
+
+    this.sinon.stub(docker, 'pull');
+    docker.pull.returns(mockedStream);
+    image.docker = docker;
+
+    setTimeout(() => {
+      mockedStream.emit('end');
+    }, 1000);
+
     await image.pull();
   });
 
-  it('should pull image only if it is not present', async () => {
+  it.only('should pull image only if it is not present', async function() {
     const imageName = 'alpine';
     const dockerImage = await docker.getImage(imageName);
     try {
@@ -52,6 +58,8 @@ describe('Image', function main() {
     }
 
     const image = new Image(imageName);
+
+    this.sinon.spy(docker, 'pull');
     image.docker = docker;
 
     await image.pull();
